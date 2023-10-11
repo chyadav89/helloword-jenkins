153001bbfb575dc02e21e4d7fc96604a7cec30d5
chyadav89 <128453169+chyadav89@users.noreply.github.com>
2023-10-11 12:20:06 +0530

    Update App.java
    webhook first commited

--- src/main/java/com/jenkins/test/App.java
+++ src/main/java/com/jenkins/test/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World! done by Mr. Chandan Yadav, Date:05/10/2023 " );
+        System.out.println( "Hello World! done by Mr. Chandan Yadav, github webhook " );
     }
 }
